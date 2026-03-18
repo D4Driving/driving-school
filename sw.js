b90b956db08acea79a11000fb5e4b357c016725b
D4Driving <info@d4driving.co.uk>
2026-03-18 12:15:13 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,56 @@
-const CACHE_NAME = 'd4driving-v3'; // Changed to v3
-const assets = [
+const CACHE_NAME = 'd4driving-v3'; // Increment this whenever you make a big layout change
+const OFFLINE_URL = 'index.html';
+
+// Initial assets to cache for the "App" to work
+const PRE_CACHE_ASSETS = [
   './',
   './index.html',
+  './blog.html',
   './tips.html',
   './400dpiLogoCropped.png',
-  './robert2.png', // Added the new image here
-  './pass1.jpg',
-  './pass8.jpg',
-  './pass10.jpg'
+  './robert2.png',
+  './manifest.json'
 ];
-// ... rest of the code stays the same
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(assets);
+// Install Event: Cache the core files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(PRE_CACHE_ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate Event: Clean up old versions
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
     })
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
+// Fetch Event: The "Soro-Friendly" Strategy
+// It looks for the file online first (so users see new blog posts), 
+// but saves a copy for offline use immediately.
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        // If it's a valid response, clone it and put it in the cache
+        const resClone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, resClone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((res) => res))
   );
 });
